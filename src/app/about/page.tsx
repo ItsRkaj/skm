b1bf25acf94088fd66ea7204a0c23c7828494f04
HasanAlzubeidi <97052700+HasanAlzubeidi@users.noreply.github.com>
2024-09-24 15:48:10 +0200

    add new section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,28 +2,59 @@ import Image from 'next/image';
 
 export default function About() {
   return (
-    <div className="flex flex-col md:flex-row items-center gap-5 p-5">
-      <div className="w-full">
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Om SKM
-        </h1>
-        <p>
-          This is the contact page. This is the contact page.This is the contact
-          page.This is the contact page.This is the contact page.This is the
-          contact page.This is the contact page.This is the contact page.This is
-          the contact page.This is the contact page.This is the contact
-          page.This is the contact page.
-        </p>
+    <div className="flex flex-col gap-10 p-5">
+      <div className="flex flex-col md:flex-row items-center gap-5 p-5">
+        <div className="w-full">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Om SKM
+          </h1>
+          <p>
+            This is the contact page. This is the contact page.This is the
+            contact page.This is the contact page.This is the contact page.This
+            is the contact page.This is the contact page.This is the contact
+            page.This is the contact page.This is the contact page.This is the
+            contact page.This is the contact page.
+          </p>
+        </div>
+
+        <div className="w-full">
+          <Image
+            src={'/img.jpeg'}
+            alt="image does not exist"
+            width={500}
+            height={200}
+            className="w-full h-80 object-cover rounded-lg mb-6"
+          />
+        </div>
       </div>
 
-      <div className="w-full">
-        <Image
-          src={'/img.jpeg'}
-          alt="image does not exist"
-          width={500}
-          height={200}
-          className="w-full h-80 object-cover rounded-lg mb-6"
-        />
+      <div className="flex flex-col md:flex-row items-center gap-5 p-5">
+        <div className="flex flex-col items-center w-full gap-5 p-5 ">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Antalet event
+          </h1>
+          <h2 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            100
+          </h2>
+        </div>
+
+        <div className="flex flex-col items-center w-full gap-5 p-5 ">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Grundades
+          </h1>
+          <h2 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            2020
+          </h2>
+        </div>
+
+        <div className="flex flex-col items-center w-full gap-5 p-5 ">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Medlemmar
+          </h1>
+          <h2 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            100
+          </h2>
+        </div>
       </div>
     </div>
   );
