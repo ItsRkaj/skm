2b11e19594e9efa0d553fff09ef9e581fc4853c1
HasanAlzubeidi <97052700+HasanAlzubeidi@users.noreply.github.com>
2024-09-24 14:42:35 +0200

    Add one section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,30 @@
-export default function Contact() {
+import Image from 'next/image';
+
+export default function About() {
   return (
-    <div>
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Om oss
-      </h1>
-      <p>Here information about SKM</p>
+    <div className="flex flex-col md:flex-row items-center gap-5 p-5">
+      <div className="w-full">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Om SKM
+        </h1>
+        <p>
+          This is the contact page. This is the contact page.This is the contact
+          page.This is the contact page.This is the contact page.This is the
+          contact page.This is the contact page.This is the contact page.This is
+          the contact page.This is the contact page.This is the contact
+          page.This is the contact page.
+        </p>
+      </div>
+
+      <div className="w-full">
+        <Image
+          src={'/img.jpeg'}
+          alt="image does not exist"
+          width={500}
+          height={200}
+          className="w-full h-80 object-cover rounded-lg mb-6"
+        />
+      </div>
     </div>
   );
 }
