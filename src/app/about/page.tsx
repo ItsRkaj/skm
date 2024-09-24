4d6dce9ab21bd50887fa5ed31a4947fbb048f618
HasanAlzubeidi <97052700+HasanAlzubeidi@users.noreply.github.com>
2024-09-24 16:48:32 +0200

    add image at the begining

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,24 @@ import Image from 'next/image';
 export default function About() {
   return (
     <div className="flex flex-col gap-10 p-5">
+      <div className="flex flex-col items-center gap-5 p-5">
+        <div className="flex flex-col items-center w-full">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            SKM
+          </h1>
+        </div>
+
+        <div className="w-full">
+          <Image
+            src={'/img.jpeg'}
+            alt="image does not exist"
+            width={500}
+            height={200}
+            className="w-full h-80 object-cover rounded-lg mb-6"
+          />
+        </div>
+      </div>
+
       <div className="flex flex-col md:flex-row items-center gap-5 p-5">
         <div className="w-full">
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
@@ -69,6 +87,31 @@ export default function About() {
             about grottan. This is the about grottan. This is the about grottan.
             This is the about grottan. This is the about grottan. This is the
             about grottan. This is the about grottan. This is the about grottan.
+          </p>
+        </div>
+
+        <div className="w-full">
+          <Image
+            src={'/img.jpeg'}
+            alt="image does not exist"
+            width={500}
+            height={200}
+            className="w-full h-80 object-cover rounded-lg mb-6"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center gap-5 p-5">
+        <div className="w-full">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Om ovve
+          </h1>
+          <p>
+            This is the about ovve. This is the about ovve. This is the about
+            ovve. This is the about ovve. This is the about ovve. This is the
+            about ovve. This is the about ovve. This is the about ovve. This is
+            the about ovve. This is the about ovve. This is the about ovve. This
+            is the about ovve.
           </p>
         </div>
 
