21fbca5b65b030e0b2c0e0322c753a03100b46c9
Ahmed Alkhalaf <ahmedalkhalaf359@gmail.com>
2024-09-27 14:29:09 +0200

    fixed the yarn format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import HeroSection from '@/components/landing-page/heroSection';
-import EventsSection from '@/components/landing-page/comingEvents';
-import News from '@/components/landing-page/newsSection';
-import AboutUs from '@/components/landing-page/aboutUs';
-import FAQ from '@/components/landing-page/FAQSection';
-import CallToAction from '@/components/landing-page/callToAction';
+import HeroSection from '@/components/landing-page/hero-section';
+import EventsSection from '@/components/landing-page/coming-events';
+import News from '@/components/landing-page/news-section';
+import AboutUs from '@/components/landing-page/about-us';
+import FAQ from '@/components/landing-page/faq-section';
+import CallToAction from '@/components/landing-page/call-to-action';
 import Footer from '@/components/landing-page/footer';
 import '@/app/globals.css';
 export default function Home() {
