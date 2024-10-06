6809bbe8bece940053cbe7d609f489099ccaa4b2
HasanAlzubeidi <97052700+HasanAlzubeidi@users.noreply.github.com>
2024-10-06 15:21:05 +0200

    add UI Quote

--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { Card, CardHeader, CardContent, CardTitle, CardDescription } from '@/components/ui/QuoteUI';
 import { getQuotes } from '@/modules/apiClient';
 import type { Quote } from '@/modules/apiTypes';
 import { useEffect, useState } from 'react';
@@ -12,29 +12,34 @@ export default function Quotes() {
     setQuotes( quoteList);
   };
 
+  
+
+
   useEffect(() => {
     void fetchQuotes();
   }, []);
 
+
   return (
-    <div>
+    <div className='flex flex-col gap-10 p-5'>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Quotes
       </h1>
-      <ul>
-        {quotes?.map((quote: Quote) => (
-          <li key={quote.author}>
-            <div className="flex items-center mb-2">
-
-              <div className="ml-2">
-                <h2>Quote: {quote.quotetext}</h2>
-                <p>Author: {quote.author}</p>
-               
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div className="space-y-5"> 
+        {quotes?(
+          quotes.map((quote) => (
+            <Card className="mb-4">
+                  <CardHeader>
+                       <CardTitle>{quote.quotetext}</CardTitle>
+                       <CardDescription>{quote.author}</CardDescription>
+                     </CardHeader>
+                   </Card>
+          ))
+        ) : (
+          <p>No quotes found</p>
+        )}
+      </div>
     </div>
   );
 }
+
