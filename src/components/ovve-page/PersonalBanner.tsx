3e04ce5da26cc59f13183dff6c026aa74d6444b1
mdih16 <91021533+mdih16@users.noreply.github.com>
2024-10-15 02:09:14 +0200

    added toast to ovve-page

--- src/components/ovve-page/PersonalBanner.tsx
+++ src/components/ovve-page/PersonalBanner.tsx
@@ -15,6 +15,7 @@ import { Input } from '@/components/ui/input';
 import { Button } from '../ui/button';
 import { useUser } from '@/context/UserContext';
 import { putLeaderboard } from '@/modules/apiClient';
+import { useToast } from '@/hooks/use-toast';
 
 interface InputFieldProps {
   label: string;
@@ -56,6 +57,7 @@ const PersonalBanner: React.FC<PersonalBannerProps> = ({
   updateLeaderboard,
 }) => {
   const { user } = useUser();
+  const { toast } = useToast();
 
   const personalData = leaderboard.find(
     (entry) => entry.person.id === user?.id,
@@ -97,8 +99,23 @@ const PersonalBanner: React.FC<PersonalBannerProps> = ({
   const handleSave = async () => {
     setLoading(true);
     try {
-      /* eslint-disable-next-line */
-      await putLeaderboard(user.id!, sewnPatches, notSewnPatches, medals, pins);
+      const success = await putLeaderboard(
+        /* eslint-disable-next-line */
+        user.id!,
+        sewnPatches,
+        notSewnPatches,
+        medals,
+        pins,
+      );
+      if (success) {
+        toast({
+          description: 'Poäng uppdaterad!',
+        });
+      } else {
+        toast({
+          description: 'Något gick fel.',
+        });
+      }
       setOriginalValues({
         sewn_patches: sewnPatches,
         not_sewn_patches: notSewnPatches,
